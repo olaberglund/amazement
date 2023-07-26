e1f2f1c3be3b06573d4713a5c40d26b1505bda31
ola berglund <olakjberglund@gmail.com>
2023-07-26 23:32:03 +0200

    finish algorithm

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Algebra.Graph.Undirected (Graph, edge, overlay, vertex, vertices)
 import qualified Algebra.Graph.Undirected as G
 import Control.Arrow (Arrow (first, second))
+import Data.List (iterate')
 import Data.Sequence (iterateN)
 import Data.Set (Set, insert, notMember)
 import qualified Data.Set as S
@@ -11,10 +12,14 @@ import System.Random (StdGen)
 import qualified System.Random as R
 import Prelude hiding (Left, Right)
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
-
 type Location = (Int, Int)
+
+data MazeState = MazeState
+  { maze :: Maze,
+    location :: Location,
+    visited :: Set Location,
+    gen :: StdGen
+  }
 
 type Maze = Graph Location
 
@@ -28,15 +33,54 @@ type Maze = Graph Location
 -- --  If the neighbor has not yet been visited,
 -- --  add the traveled edge to the spanning tree.
 -- --  Repeat step 2 until all vertexes have been visited.
+width = 10
 
-aldousStep :: (StdGen, Maze, Location, Set Location) -> (StdGen, Maze, Location, Set Location)
-aldousStep (gen, maze, location, visited) =
-  if notMember location' visited
-    then (gen', overlay (edge location location') maze, location', insert location' visited)
-    else (gen', maze, location', visited)
+height = 10
+
+initialMaze :: MazeState
+initialMaze =
+  let startLocation = (0, 0)
+   in MazeState
+        { maze = G.empty,
+          location = startLocation,
+          visited = S.singleton startLocation,
+          gen = R.mkStdGen 10
+        }
+
+main :: IO ()
+main = do
+  let m = iterateUntil haveVisitedAll aldousStep initialMaze
+  print (maze m)
   where
-    (dir, gen') = randomDirection gen
-    location' = move dir location
+    haveVisitedAll :: MazeState -> Bool
+    haveVisitedAll m = S.size (visited m) == width * height
+
+-- https://www.w3.org/TR/xml-entity-names/025.html
+
+iterateUntil :: (a -> Bool) -> (a -> a) -> a -> a
+iterateUntil p f = head . filter p . iterate' f
+
+aldousStep :: MazeState -> MazeState
+aldousStep m =
+  if notMember loc' (visited m)
+    then
+      m
+        { gen = gen',
+          maze = overlay (edge (location m) loc') (maze m),
+          location = loc',
+          visited = insert loc' (visited m)
+        }
+    else m {gen = gen', location = loc'}
+  where
+    (loc', gen') = findLegalMove (gen m) (location m)
+
+findLegalMove :: StdGen -> Location -> (Location, StdGen)
+findLegalMove gen loc =
+  let (dir, gen') = randomDirection gen
+      loc' = move dir loc
+   in if withinBounds loc' then (loc', gen') else findLegalMove gen' loc
+  where
+    withinBounds (x, y) = x >= 0 && x < width && y >= 0 && y < height
 
 randomDirection :: StdGen -> (Direction, StdGen)
 randomDirection = first toEnum . R.randomR (0 :: Int, fromEnum (maxBound :: Direction))
@@ -48,6 +92,3 @@ move Up = second succ
 move Right = first succ
 move Down = second pred
 move Left = first pred
-
--- generateRandomMaze :: StdGen -> (Int, Int) -> M.Map Location CellBoundaries
--- generateRandomMaze gen (numRows, numColumns) = undefined
