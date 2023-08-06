2822aeee81b6b389b05e102fb2b1e4fe68adc89d
Ola Berglund <olakjberglund@gmail.com>
2023-08-06 21:39:28 +0200

    show diff time at goal

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
+import Control.Monad (replicateM_)
+import qualified Control.Monad
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Set as S
-import Data.Text (unpack)
+import qualified Data.Text as T
+import Data.Time
 import Generators (aldousStep)
 import Showing
 import Solvers (backtracker, start)
@@ -20,33 +23,41 @@ main = runInputT defaultSettings $ do
   n <- getInputLine "Seed: "
   liftIO clearScreen
   liftIO $ hSetBuffering stdin NoBuffering >> hSetEcho stdin False
+  startTime <- liftIO getCurrentTime
   case n of
     Nothing -> return ()
     Just n -> gameLoop (generateMaze aldousStep (read n))
+  endTime <- liftIO getCurrentTime
+  outputStrLn $ "Time: " ++ show (diffUTCTime endTime startTime)
   where
     gameLoop :: MazeState -> InputT IO ()
     gameLoop ms = do
       let m = maze ms
           ploc = player ms
-      outputStrLn $ unpack $ showMaze ms
-      key <- getInputChar ""
-      case key of
-        Just 'w' -> gameLoop (ms {player = moveLegally ploc Up m})
-        Just 's' -> gameLoop (ms {player = moveLegally ploc Down m})
-        Just 'd' -> gameLoop (ms {player = moveLegally ploc Right m})
-        Just 'a' -> gameLoop (ms {player = moveLegally ploc Left m})
-        -- cheatings
-        Just 'k' -> gameLoop (ms {player = move Up ploc})
-        Just 'j' -> gameLoop (ms {player = move Down ploc})
-        Just 'l' -> gameLoop (ms {player = move Right ploc})
-        Just 'h' -> gameLoop (ms {player = move Left ploc})
-        Just 'r' -> gameLoop (ms {player = (0, 0)})
-        Just 'g' -> gameLoop (ms {player = (width - 1, height - 1)})
-        Just 'p' -> gameLoop (ms {showPath = not (showPath ms)})
-        -- meta
-        Just 'n' -> liftIO clearScreen >> liftIO main
-        Just 'q' -> return ()
-        _ -> gameLoop ms
+          mazeHeight = length $ T.lines $ showMaze ms
+      replicateM_ (58 - mazeHeight) (outputStrLn "")
+      outputStrLn $ T.unpack $ showMaze ms
+      if ploc == (width - 1, height - 1)
+        then return ()
+        else do
+          key <- getInputChar ""
+          case key of
+            Just 'w' -> gameLoop (ms {player = moveLegally ploc Up m})
+            Just 's' -> gameLoop (ms {player = moveLegally ploc Down m})
+            Just 'd' -> gameLoop (ms {player = moveLegally ploc Right m})
+            Just 'a' -> gameLoop (ms {player = moveLegally ploc Left m})
+            -- cheating
+            Just 'k' -> gameLoop (ms {player = move Up ploc})
+            Just 'j' -> gameLoop (ms {player = move Down ploc})
+            Just 'l' -> gameLoop (ms {player = move Right ploc})
+            Just 'h' -> gameLoop (ms {player = move Left ploc})
+            Just 'r' -> gameLoop (ms {player = (0, 0)})
+            Just 'g' -> gameLoop (ms {player = (width - 1, height - 1)})
+            Just 'p' -> gameLoop (ms {showPath = not (showPath ms)})
+            -- meta
+            Just 'n' -> liftIO clearScreen >> liftIO main
+            Just 'q' -> return ()
+            _ -> gameLoop ms
 
 generateMaze :: (MazeState -> MazeState) -> Int -> MazeState
 generateMaze stepper n =
